67e6599c1baf00d57610dec109a762f6e121d8a4
Amir <amirhamza11793@gmail.com>
2022-12-05 12:22:00 +0530

    added public and private route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
-import styles from './styles/main.scss';
+import { Switch } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute.jsx'
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div>hello</div>
+    <Switch>
+      <PublicRoute path='/signin' >
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path='/' >
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
