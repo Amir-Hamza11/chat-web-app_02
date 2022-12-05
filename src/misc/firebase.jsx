d02a50627ec249b7cde13e6013ced16975f22250
Amir <amirhamza11793@gmail.com>
2022-12-05 11:39:30 +0530

    firebase addeded to the project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+
+const config = {
+  apiKey: "AIzaSyBST8lsDThNASWfAIHg0tZRUtrZ4hsRhfU",
+  authDomain: "chat-web-app-f4df3.firebaseapp.com",
+  databaseURL: "https://chat-web-app-f4df3-default-rtdb.firebaseio.com",
+  projectId: "chat-web-app-f4df3",
+  storageBucket: "chat-web-app-f4df3.appspot.com",
+  messagingSenderId: "351785435364",
+  appId: "1:351785435364:web:10f9b3c81d6f4053a56cf1"
+};
+
+const app = firebase.initializeApp(config);
