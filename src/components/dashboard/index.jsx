71a7f20412085bcd7f881f490beec141f9969510
Amir <amirhamza11793@gmail.com>
2022-12-08 10:41:32 +0100

    updated nickname inside database

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,42 +1,51 @@
 
 import React from 'react'
-import { Button, Divider, Drawer } from 'rsuite'
+import { Alert, Button, Divider, Drawer } from 'rsuite'
 import { useProfile } from '../../context/ProfileContext'
+import { database } from '../../misc/firebase'
 import EditableInput from '../EditableInput'
 
 
-const Dashboard = ({onSignOut}) => {
+const Dashboard = ({ onSignOut }) => {
 
-    const {profile} = useProfile()
-    const onSave = async (newData)=> {
-        console.log(newData);
+    const { profile } = useProfile()
+    const onSave = async (newData) => {
+        const userNicknameRef = database.ref(`/profiles/${profile.uid}`).child('name');
+
+        try {
+            await userNicknameRef.set(newData);
+
+            Alert.success('Nickname has been updated', 4000);
+        } catch (err) {
+            Alert.error('err.message', 4000);
+        }
     }
 
-  return <>
-    <Drawer.Header>
-        <Drawer.Title>
-            Dashboard
-        </Drawer.Title>
-    </Drawer.Header>
+    return <>
+        <Drawer.Header>
+            <Drawer.Title>
+                Dashboard
+            </Drawer.Title>
+        </Drawer.Header>
 
-    <Drawer.Body>
-    <h3>hey, {profile.name}</h3>
-    <Divider/>
-    <EditableInput 
-        name='nickname'
-        initialValue={profile.name}
-        onSave={onSave}
-        label={<h6 className='mb-2' >Nickname</h6>}
-    />
-    </Drawer.Body>
+        <Drawer.Body>
+            <h3>hey, {profile.name}</h3>
+            <Divider />
+            <EditableInput
+                name='nickname'
+                initialValue={profile.name}
+                onSave={onSave}
+                label={<h6 className='mb-2' >Nickname</h6>}
+            />
+        </Drawer.Body>
 
-    <Drawer.Footer>
-        <Button block color='red' onClick={onSignOut} >
-            Sign out
-        </Button>
-    </Drawer.Footer>
-  </>
-  
+        <Drawer.Footer>
+            <Button block color='red' onClick={onSignOut} >
+                Sign out
+            </Button>
+        </Drawer.Footer>
+    </>
+
 }
 
 export default Dashboard
